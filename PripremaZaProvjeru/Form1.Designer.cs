89d2933a4e9fc205fb1f08b18b280ea496549141
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-03-01 12:07:38 +0100

    Add files via upload

---
+++ PripremaZaProvjeru/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PripremaZaProvjeru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbVrsta = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNaziv = new System.Windows.Forms.TextBox();
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpGodPro = new System.Windows.Forms.DateTimePicker();
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.btnIspisi = new System.Windows.Forms.Button();
+            this.trckSnaga = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rtbIspis = new System.Windows.Forms.RichTextBox();
+            this.lblSnaga = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trckSnaga)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbVrsta
+            // 
+            this.cmbVrsta.FormattingEnabled = true;
+            this.cmbVrsta.Items.AddRange(new object[] {
+            "Automobil",
+            "JetSki",
+            "Zrakomlat",
+            ""});
+            this.cmbVrsta.Location = new System.Drawing.Point(113, 103);
+            this.cmbVrsta.Name = "cmbVrsta";
+            this.cmbVrsta.Size = new System.Drawing.Size(231, 21);
+            this.cmbVrsta.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vrsta vozila";
+            // 
+            // txtNaziv
+            // 
+            this.txtNaziv.Location = new System.Drawing.Point(113, 143);
+            this.txtNaziv.Name = "txtNaziv";
+            this.txtNaziv.Size = new System.Drawing.Size(231, 20);
+            this.txtNaziv.TabIndex = 2;
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Location = new System.Drawing.Point(113, 180);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(231, 20);
+            this.txtMarka.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Naziv vozila";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Marka vozila";
+            // 
+            // dtpGodPro
+            // 
+            this.dtpGodPro.Location = new System.Drawing.Point(144, 220);
+            this.dtpGodPro.Name = "dtpGodPro";
+            this.dtpGodPro.Size = new System.Drawing.Size(200, 20);
+            this.dtpGodPro.TabIndex = 6;
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.Location = new System.Drawing.Point(425, 101);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(155, 95);
+            this.btnUnesi.TabIndex = 7;
+            this.btnUnesi.Text = "Unesi";
+            this.btnUnesi.UseVisualStyleBackColor = true;
+            this.btnUnesi.Click += new System.EventHandler(this.btnUnesi_Click);
+            // 
+            // btnIspisi
+            // 
+            this.btnIspisi.Location = new System.Drawing.Point(598, 101);
+            this.btnIspisi.Name = "btnIspisi";
+            this.btnIspisi.Size = new System.Drawing.Size(152, 95);
+            this.btnIspisi.TabIndex = 8;
+            this.btnIspisi.Text = "Ispiši";
+            this.btnIspisi.UseVisualStyleBackColor = true;
+            this.btnIspisi.Click += new System.EventHandler(this.btnIspisi_Click);
+            // 
+            // trckSnaga
+            // 
+            this.trckSnaga.LargeChange = 20;
+            this.trckSnaga.Location = new System.Drawing.Point(113, 264);
+            this.trckSnaga.Maximum = 350;
+            this.trckSnaga.Minimum = 45;
+            this.trckSnaga.Name = "trckSnaga";
+            this.trckSnaga.Size = new System.Drawing.Size(506, 45);
+            this.trckSnaga.SmallChange = 5;
+            this.trckSnaga.TabIndex = 9;
+            this.trckSnaga.Value = 45;
+            this.trckSnaga.Scroll += new System.EventHandler(this.trckSnaga_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Godina proizvodnje";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Snaga(KS)";
+            // 
+            // rtbIspis
+            // 
+            this.rtbIspis.Location = new System.Drawing.Point(43, 315);
+            this.rtbIspis.Name = "rtbIspis";
+            this.rtbIspis.Size = new System.Drawing.Size(707, 205);
+            this.rtbIspis.TabIndex = 12;
+            this.rtbIspis.Text = "";
+            // 
+            // lblSnaga
+            // 
+            this.lblSnaga.AutoSize = true;
+            this.lblSnaga.Location = new System.Drawing.Point(639, 270);
+            this.lblSnaga.Name = "lblSnaga";
+            this.lblSnaga.Size = new System.Drawing.Size(19, 13);
+            this.lblSnaga.TabIndex = 13;
+            this.lblSnaga.Text = "45";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 552);
+            this.Controls.Add(this.lblSnaga);
+            this.Controls.Add(this.rtbIspis);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trckSnaga);
+            this.Controls.Add(this.btnIspisi);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.dtpGodPro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMarka);
+            this.Controls.Add(this.txtNaziv);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbVrsta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trckSnaga)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbVrsta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNaziv;
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpGodPro;
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.Button btnIspisi;
+        private System.Windows.Forms.TrackBar trckSnaga;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtbIspis;
+        private System.Windows.Forms.Label lblSnaga;
+    }
+}
+
---
+++ PripremaZaProvjeru/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PripremaZaProvjeru
+{
+    
+    public partial class Form1 : Form
+    {
+        List<Vozilo> listaVozila = new List<Vozilo>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            Vozilo vozilo = new Vozilo(cmbVrsta.Text,txtNaziv.Text,txtMarka.Text,dtpGodPro.Value,trckSnaga.Value);
+
+            listaVozila.Add(vozilo);
+            MessageBox.Show("Uspješan unos","Uspjeh",MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void trckSnaga_Scroll(object sender, EventArgs e)
+        {
+            lblSnaga.Text = trckSnaga.Value.ToString();
+        }
+
+        private void btnIspisi_Click(object sender, EventArgs e)
+        {
+            rtbIspis.Clear();
+            foreach (Vozilo vozilo in listaVozila)
+            {
+                rtbIspis.AppendText(vozilo.ToString());
+            }
+        }
+    }
+}
